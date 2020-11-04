c758eb7cedd6a1eadaeb8d54d130a47ff324cb40
kalanasa1996s <kalanasa1996@gmail.com>
2020-11-04 21:08:42 +0530

    Day 2 - Project

---
+++ Day 2/Project 3/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        WebServer webServer = new WebServer();
+        webServer.CreateServer();
+
+        try {
+            webServer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
